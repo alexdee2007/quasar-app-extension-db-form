ea765530cc803b288af84dd34c775ec4b41bac94
o.diachenko.2005 <o.diachenko.2005@in.np.gov.ua>
2020-04-18 16:44:05 +0300

    wip

--- src/component/DbForm.vue
+++ src/component/DbForm.vue
@@ -31,37 +31,16 @@
         type: Object,
         default: () => ({})
       },
-      modelName: {
-        type: String,
-        required: true
-      },
-      isDirty: Boolean,
-      extraValidations: {
-        type: Object,
-        default: () => ({})
-      },
+      //isDirty: Boolean,
+      //extraValidations: {
+      //  type: Object,
+      //  default: () => ({})
+      //},
       saveOnSubmit: Boolean
-    },
-    computed: {
-      model() {
-        return models[this.modelName];
-      },
-      canLeave() {
-        return this.notChanged;
-      },
-      notChanged() {
-        return isEqualWith(this.value, this.loadedValue, equalModelValues);
-      },
-      isEmpty() {
-        return isEqualWith(this.value, this.initialValue, equalModelValues);
-      },
-      hasErrors() {
-        return this.$v.$error;
-      }
     },
     data() {
       return {
-        loadedValue: {},
+        //loadedValue: {},
         validationValue: {}
       }
     },
@@ -99,53 +78,60 @@
         this.model && iterator(this.model.fields);
         return validations;
       },
-      async save() {
-        try {
-          this.$q.loading.show({message: 'Збереження...'});
-          const data = await this.$api.model.save(this.modelName, this.value);
-          this.loadedValue = isEmpty(this.initialValue) ? merge({}, this.value, data) : pick(data, deepKeys(this.initialValue));
-          this.$q.notify({color: 'positive', timeout: 2500, message: 'Дані успішно збережно', position: 'top', icon: 'done'});
-          this.$emit('update:value', cloneDeep(this.loadedValue));
-          this.$emit('save', cloneDeep(this.loadedValue));
-        } catch (err) {
-          console.error(err);
-        } finally {
-          this.$q.loading.hide();
-        }
-      },
+      /*
+       async save() {
+       try {
+       this.$q.loading.show({message: 'Збереження...'});
+       const data = await this.$api.model.save(this.modelName, this.value);
+       this.loadedValue = isEmpty(this.initialValue) ? merge({}, this.value, data) : pick(data, deepKeys(this.initialValue));
+       this.$q.notify({color: 'positive', timeout: 2500, message: 'Дані успішно збережно', position: 'top', icon: 'done'});
+       this.$emit('update:value', cloneDeep(this.loadedValue));
+       this.$emit('save', cloneDeep(this.loadedValue));
+       } catch (err) {
+       console.error(err);
+       } finally {
+       this.$q.loading.hide();
+       }
+       },*/
       async submit(evt) {
+
         evt && stopAndPrevent(evt);
-        this.$v.$touch();
-        if (this.$v.$error) {
+        this.value.$validate.$touch();
+
+        if (this.value.$validate.$error) {
           return false;
         }
-	this.$v.$reset();
+
+        this.value.$validate.$reset();
         this.beforeSubmit && await this.beforeSubmit(this.value);
+        await this.value.commit(this.saveOnSubmit);
         this.$emit('submit');
-        this.saveOnSubmit && await this.save();
       },
       reset(evt) {
         evt && stopAndPrevent(evt);
-        this.$emit('update:value', cloneDeep(this.initialValue));
-        this.$v.$reset();
+        this.value.$reset();
         this.$emit('reset');
       },
+
       cancel(evt) {
         evt && stopAndPrevent(evt);
-        this.$emit('update:value', cloneDeep(this.loadedValue));
-        this.$v.$reset();
+
+        this.value.$rollback();
         this.$emit('cancel');
       }
     },
+
     validations() {
-      return {value: this.validationValue};
+      return {[this.value.$options.name]: this.validationValue};
     },
     async mounted() {
-      this.$emit('update:value', merge({}, this.initialValue, this.value));
-      await this.$nextTick();
-      this.model && (this.validationValue = merge({}, this.getValidationsRecursively(), get(this.extraValidations, this.modelName)));
-      this.isDirty && this.$v.$touch();
-      this.loadedValue = cloneDeep(this.value);
+      // console.log(this.value);
+      //this.$emit('update:value', merge({}, this.initialValue, this.value));
+      //await this.$nextTick();
+      //this.model && (this.validationValue = merge({}, this.getValidationsRecursively(), get(this.extraValidations, this.modelName)));
+      //this.isDirty && this.$v.$touch();
+      //this.loadedValue = cloneDeep(this.value);
+     // this.validationValue = merge({}, this.getValidationsRecursively(), get(this.extraValidations, this.value.$options.name))
     }
   }
 </script>
